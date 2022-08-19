00c2d57228fcb97c3dcc731d3fc1e0b97c6e9f95
arthurariza <tucaariza@gmail.com>
2022-08-19 18:28:49 -0300

    add truncated text to Articles

--- app/resources/api/v1/article_resource.rb
+++ app/resources/api/v1/article_resource.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Api::V1::ArticleResource < JSONAPI::Resource
-  attributes :title, :full_text, :image, :created_at, :updated_at
+  attributes :title, :text, :full_text, :image, :created_at, :updated_at
+
+  def text
+    @model.full_text.truncate(200)
+  end
 end
